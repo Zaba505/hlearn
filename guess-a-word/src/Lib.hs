cef74d1f24c21b123bec7d1467ef0c2d1151c0c6
Zaba505 <cakub6@gmx.com>
2022-02-19 18:46:57 -0500

    feat(guess-a-word): mask unguessed characters in word (#5)

--- guess-a-word/src/Lib.hs
+++ guess-a-word/src/Lib.hs
@@ -7,8 +7,17 @@ import           Data.List
 printChar :: Char -> IO ()
 printChar = putStrLn . flip (:) []
 
+maskUnguessedChars :: String -> [Char] -> String
+maskUnguessedChars word guesses = reverse $ foldr (mask guesses) [] word
+  where
+    mask :: [Char] -> Char -> String -> String
+    mask guesses letter maskedWord = if elem letter guesses
+      then maskedWord ++ [letter] ++ " "
+      else maskedWord ++ "_ "
+
 runGame :: [String] -> IO ()
 runGame ws = do
+  putStrLn $ maskUnguessedChars (ws !! 0) []
   putStrLn "Guess a letter:"
   fmap (\s -> ([], s !! 0)) getLine >>= runGame' (ws !! 0)
   where
@@ -18,7 +27,10 @@ runGame ws = do
           putStrLn "already guessed that letter"
           fmap (\s -> ([], s !! 0)) getLine >>= runGame' (ws !! 0)
         else if length (nub word) == length (intersect word guesses)
-          then putStrLn "you win"
+          then do
+            putStrLn $ maskUnguessedChars word (guess:guesses)
+            putStrLn "you win"
           else do
+            putStrLn $ maskUnguessedChars word (guess:guesses)
             putStrLn "Guess a letter:"
             fmap (\s -> ((guess:guesses), s !! 0)) getLine >>= runGame' word
